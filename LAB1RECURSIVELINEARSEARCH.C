6376bc365e3fa715b59b91398445c3b686bcb20d
shivanshuserpentine2001 <63629636+shivanshuserpentine2001@users.noreply.github.com>
2021-04-15 12:00:17 +0530

    Create LAB1RECURSIVELINEARSEARCH.C
    LAB1 WEEK1

---
+++ LAB1RECURSIVELINEARSEARCH.C
@@ -0,0 +1,41 @@
+// C program to implement recursive Binary Search
+#include <stdio.h>
+  
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+  
+      
+        if (arr[mid] == x)
+            return mid;
+  
+        
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        return binarySearch(arr, mid + 1, r, x);
+    }
+  
+  
+    return -1;
+}
+  
+int main(void)
+{
+    int i, arr[5];
+ 
+    int x ;
+    printf("ENTER NO TO BE SEARCHED:");
+        scanf("%d",&x);
+    
+    for(i=0;i<5;i++)
+    {scanf("%d",&arr[i]);}
+       int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1) ? printf("Element is not present in array")
+                   : printf("Element is present at index %d",
+                            result);
+    return 0;
+}
