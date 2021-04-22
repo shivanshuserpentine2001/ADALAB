0af948a84c5e0615d5e96bc918dbba79987f1577
shivanshuserpentine2001 <63629636+shivanshuserpentine2001@users.noreply.github.com>
2021-04-22 11:43:44 +0530

    LAB 2 WEEK 2

---
+++ BUBBLESORT.c
@@ -0,0 +1,43 @@
+#include  <stdio.h>
+#include <stdio.h>
+  
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+  
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+  
+       // Last i elements are already in place   
+       for (j = 0; j < n-i-1; j++) 
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+  
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+  
+// Driver program to test above functions
+int main()
+{
+     int arr[11],i;
+            printf("ENTER ELEMTTS OF ARRAY(MAX OF 10)");
+    for(i=0;i<11;i++)
+    {scanf("%d",&arr[i])    ;}
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array(BUBBLE): \n");
+    printArray(arr, n);
+    return 0;
+}
